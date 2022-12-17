48dd3550816fff5754bfcaf68a117f5197957c70
allelleo <dev.allelleo@internet.ru>
2022-12-17 13:19:05 +0300

    commit #2

--- eatsite/home/models.py
+++ eatsite/home/models.py
@@ -9,18 +9,43 @@ class Category(models.Model):
     views = models.IntegerField(default=0)
 
 
+class CookingLevel(models.Model):
+    title = models.CharField(max_length=100)
+
+
+class KitchenType(models.Model):
+    title = models.CharField(max_length=100)
+
+
 class Recipe(models.Model):
-    title = models.CharField(max_length=150)
-    quote = models.CharField(max_length=300)
-    slug = models.CharField(max_length=200, null=True, blank=True)
-    published_date = models.DateTimeField(auto_now_add=True)
-    steps = models.TextField()
-    preview = models.ImageField(upload_to='preview/')
-    views = models.IntegerField(default=0)
-    tags = models.CharField(max_length=200)
-    category = models.ForeignKey(Category, on_delete=models.PROTECT)
-    author_name = models.CharField(max_length=150)
-    author_link = models.CharField(max_length=200)
+    title = models.CharField(max_length=150)  # Название рецепта
+    quote = models.CharField(max_length=300)  # Краткое описание
+    slug = models.CharField(max_length=200, null=True, blank=True)  # Ссылка на рецепт
+    published_date = models.DateTimeField(auto_now_add=True)  # Дата публикации
+
+    steps = models.TextField()  # Шаги готовки
+    ingredients = models.TextField()  # Ингредиенты
+
+    preview = models.ImageField(upload_to='preview/')  # Фото блюда
+    views = models.IntegerField(default=0)  # Количество просмотров
+    tags = models.CharField(max_length=200)  # Теги
+
+    author_name = models.CharField(max_length=150)  # Имя автора
+    author_link = models.CharField(max_length=200)  # Ссылка на автора
+
+    CookingTime = models.IntegerField(default=0)  # Время готовки
+    PortionCounter = models.IntegerField(default=1)  # Количество порций
+
+    CaloricContent = models.IntegerField(default=0)  # Калорийность
+    Squirrels = models.IntegerField(default=0)  # Белки
+    Fats = models.IntegerField(default=0)  # Жиры
+    Carbohydrates = models.IntegerField(default=0)  # Углеводы
+    Water = models.IntegerField(default=0)  # Вода
+    DietaryFiber = models.IntegerField(default=0)  # Пищевые волокна
+
+    CookingLevel = models.ForeignKey(CookingLevel, on_delete=models.PROTECT)  # Сложность готовки
+    KitchenType = models.ForeignKey(KitchenType, on_delete=models.PROTECT)  # Тип кухни
+    category = models.ForeignKey(Category, on_delete=models.PROTECT)  # категория
 
     def save(self, *args, **kwargs):
         self.slug = slugify(self.title) + str(self.pk)
--- eatsite/home/views.py
+++ eatsite/home/views.py
@@ -1,3 +1,28 @@
 from django.shortcuts import render
+from django.views import View
+
+from .apps import HomeConfig
+
 
 # Create your views here.
+class Home(View):
+    template_name = 'home/index.html'
+
+    def context(self):
+        return {
+            'SiteName': HomeConfig.SiteName,
+            "SalatRecipeCounter": 666,
+            "FishRecipeCounter": 666,
+            "ChickenRecipeCounter": 666,
+            "MeatRecipeCounter": 666,
+            "ProperNutritionRecipeCounter": 666,
+            "CoffeeRecipeCounter": 666,
+            "TeaRecipeCounter": 666,
+            "SoupRecipeCounter": 666,
+            "SnackRecipeCounter": 666,
+            "SweetRecipeCounter": 666,
+            "DrinkRecipeCounter": 666,
+        }
+
+    def get(self, request, *args, **kwargs):
+        return render(request, self.template_name, self.context())
